df46baa8b92f838f694420257bac72db259d9a3d
PaulaEsteve <paulaeg78@gmail.com>
2025-05-03 20:40:50 +0200

    primer commit

---
+++ src/main/java/org/inplasa/mantenimineto/inplasa/InplasaApplication.java
@@ -0,0 +1,13 @@
+package org.inplasa.mantenimineto.inplasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InplasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InplasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/inplasa/mantenimineto/inplasa/InplasaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inplasa.mantenimineto.inplasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InplasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
