b03e82b420706f784c7c6318a18a0f2e3c8199eb
PaulaEsteve <paulaeg78@gmail.com>
2025-05-04 17:52:36 +0200

    entidades db y dto

---
+++ src/main/java/org/inplasa/mantenimineto/inplasa/model/dto/proveedorEdit.java
@@ -0,0 +1,21 @@
+package org.inplasa.mantenimineto.inplasa.model.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class proveedorEdit implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private Long id;
+
+    @NotNull(message = "el nombre del proveedor no puede ser nulo.")
+    private String nombre;
+
+    private String descripcion;
+}
---
+++ src/main/java/org/inplasa/mantenimineto/inplasa/model/dto/proveedorList.java
@@ -0,0 +1,21 @@
+package org.inplasa.mantenimineto.inplasa.model.dto;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class proveedorList implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+
+    @NotNull(message = "el nombre del proveedor no puede ser nulo.")
+    private String nombre;
+
+    private String descripcion;
+}
